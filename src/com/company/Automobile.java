2f6d1db70bcc6aa7dcc9bb2918f6e23be72b3b59
p-ganesh <pg304@pvsd.org>
2018-10-12 10:42:56 -0400

    ;lsihdf

--- src/com/company/Automobile.java
+++ src/com/company/Automobile.java
@@ -4,4 +4,23 @@ package com.company;
  * Created by dpennebacker on 10/9/16.
  */
 public class Automobile {
+    public int milesPerGallon;
+    public double gallons=0.0;
+
+    public Automobile (int a)
+    {
+        milesPerGallon = a;
+    }
+    public void fillUp (int b)
+    {
+        gallons =+ b;
+    }
+    public void takeTrip (int c)
+    {
+        gallons = gallons - c/milesPerGallon;
+    }
+    public double reportFuel ()
+    {
+        return gallons;
+    }
 }
